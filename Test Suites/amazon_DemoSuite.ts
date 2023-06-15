75e0e27c84ddf3e4b19b1988e913359b7d58b359
Salinee <nayaksalinee@gmail.com>
2023-06-15 14:03:07 +0530

    pushing suite folder

---
+++ Test Suites/amazon_DemoSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>amazon_DemoSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f1e04902-2cfe-4d8e-a88f-3a66dcd237fa</testSuiteGuid>
+   <testCaseLink>
+      <guid>598fd58f-b7a7-457e-a0f2-4ef4f6083938</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonE2EFLow/AmazonAutomation</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
